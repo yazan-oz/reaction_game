e089f30322176861915b59250bd40fa6467fc898
Yazan Alhashimi <yazanalhashimi45@gmail.com>
2025-10-16 22:43:43 +0200

    Fixed host binding

--- app.py
+++ app.py
@@ -413,4 +413,4 @@ if __name__ == "__main__":
     
     if __name__ == '__main__':
         port = int(os.environ.get('PORT', 5000))
-        app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+        app.run(host='0.0.0.0', port=port, debug=False)
